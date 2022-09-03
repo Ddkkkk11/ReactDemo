52e3c7dc185653e9c114e95f9275ad6282899ed6
xdd <45672942@qq.com>
2022-09-03 14:03:53 +0800

    react学习

--- src/index.js
+++ src/index.js
@@ -1,17 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import  ReactDOM  from "react-dom";
+//ReactDOM.render(组件名称，要注入的标签root)
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// ReactDOM.createRoot(document.getElementById('root')).render(<h1>你好啊</h1>)
+ReactDOM.render(<h1>HelloWorld</h1>,document.getElementById('root'));
