a8fe42679670b917e23d949a7d9212d375dff561
xdd <45672942@qq.com>
2022-09-04 19:15:28 +0800

    change index

--- src/index.js
+++ src/index.js
@@ -1,12 +1,4 @@
-// import ReactDOM from 'react-dom';
 import {createRoot} from "react-dom/client";
-// import React from 'react';
 import App from "./App5";
 
 createRoot(document.getElementById('root')).render(<App/>)
-// ReactDOM.render(<App/>,document.getElementById('root'));
-// ReactDOM.creatRoot(document.getElementById('root')).render(<App/>);
-/*
-setTimeout(()=>{
-  ReactDOM.render(<input/>,document.getElementById('root'));
-},3000)*/
