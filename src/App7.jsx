5a35b6c3b9ed06eb6a2d2dd3311f8faee57ef82f
Roll(薛军栋) <roll.xue@yeahmobi.com>
2022-09-05 20:01:01 +0800

    useCallBack和memo搭配

--- src/App7.jsx
+++ src/App7.jsx
@@ -1,10 +1,12 @@
-import React, { useState, memo } from 'react'
+import React, { useState, memo, useContext, createContext,useCallback } from 'react'
 
+const Value = createContext()
 const Child = memo(() => {
-  console.log(123);
+  console.log(123)
+  const { doSth } = useContext(Value)
   return <>
     <h2>我是子组件</h2>
-
+    <button onClick={() => doSth()}>累加</button>
   </>
 
 })
@@ -19,13 +21,23 @@ function Child(){
 
 }*/
 const App7 = () => {
-  const [num, setNum] = useState(1)
+  const [num, setNum] = useState(1);
+  /*
+  * setNum(newValue)  使用新值强行覆盖初始值旧值
+  * set Num( (num) =>num+1); 不断使用新值覆盖旧值
+  *
+  *
+  *
+  * */
+  const doSth = useCallback(()=> setNum((num) =>num+1),[]);
+
   return (
     <div>
       <h2>父组件,Num为：{num}</h2>
-      <button onClick={() => setNum(num + 1)}>累加</button>
       <hr/>
-      <Child/>
+      <Value.Provider value={{ doSth } }>
+        <Child/>
+      </Value.Provider>
     </div>
   )
 }
